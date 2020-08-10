02ee991c16628f80c1a1c93a25aa9d09014cf781
Ander García Gangoiti <agarcia@vicomtech.es>
2020-08-10 09:17:47 +0200

    Add readme

--- tarea_21/main.py
+++ tarea_21/main.py
@@ -4,15 +4,15 @@ import sys
 def fraccion_irreducible (numero, referencia_decimales = 10000):
     denominador = referencia_decimales
     numerador = int(numero*denominador)
-    max_number = numerador
-    current_number = 2
-    while max_number >= current_number:
-        if (numerador%current_number) == 0 and (denominador%current_number) == 0:
-            numerador = numerador/current_number
-            denominador = denominador/current_number
-            max_number = numerador
+    max_numero = numerador
+    divisor_comun = 2
+    while max_numero >= divisor_comun:
+        if (numerador%divisor_comun) == 0 and (denominador%divisor_comun) == 0:
+            numerador = numerador/divisor_comun
+            denominador = denominador/divisor_comun
+            max_numero = numerador
         else:
-            current_number = current_number +1
+            divisor_comun = divisor_comun +1
     numerador = int (numerador)
     denominador = int (denominador)
     return {"numerador" : numerador,"denominador":denominador}
