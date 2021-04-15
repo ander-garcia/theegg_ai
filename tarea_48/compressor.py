1065290a76f8e259373b5f043c68d858629ee336
Ander García Gangoiti <agarcia@vicomtech.es>
2021-04-15 02:51:55 +0200

    Add comments

--- tarea_48/compressor.py
+++ tarea_48/compressor.py
@@ -14,23 +14,30 @@ class Compressor():
             length = 1
             best_offset = 0
             best_length = 0
+            # buscar coincidencias en los caracteres previos
             while index - offset >= 0 or offset >= self.max_window:
                 if text[index] == text[index-offset]:
                     length = 1
                     extra_length = 1
-                    while extra_length < self.max_window and index + extra_length < len(text) and text[index + extra_length] == text[index-offset+extra_length]:
+                    # si hay coincidencia se mira si los siguientes caracteres tambien son iguales
+                    while extra_length < self.max_window and \
+                        index + extra_length < len(text) and \
+                            text[index + extra_length] == text[index-offset+extra_length]:
                         extra_length = extra_length + 1
                         length = length + 1
-
+                    # si la coincidencia es más larga que una previa se guarda
                     if length > best_length:
                         best_length = length
                         best_offset = offset
                 offset = offset + 1
-                current_char = ""
+            # se genera la tupla correspondiente
+            # en formato offset,length, char (vacío si hay coincidencia previa)
+            current_char = ""
             if best_length == 0:
                 best_length = 1
                 current_char = text[index]
             output.append((best_offset, best_length, current_char))
+            # se avanza en la cadena de entrada la longitud de la coincidencia
             index = index + best_length
         return output
 
@@ -39,9 +46,13 @@ class Compressor():
         index = 0
         while index < len(compressed_text):
             offset, length, character = compressed_text[index]
+            # si no había offset se añade el caracter
             if offset == 0:
                 text = text + character
             else:
+                # si habia offset
+                # se añaden las coincidencias una a una
+                # hasta la longitud codificada
                 inserted_chars = 0
                 text_index = len(text)
                 while inserted_chars < length:
